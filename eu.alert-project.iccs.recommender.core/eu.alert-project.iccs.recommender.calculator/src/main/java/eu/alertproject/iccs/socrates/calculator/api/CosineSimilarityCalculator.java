61d7a42cdb79dbd4bbb4edb0c7a2581f79017b2f
Kostas Christidis <costas.x@gmail.com>
2012-05-16 10:51:41 +0300

    Calculation with Map<Integer,Integer> and testing

--- eu.alert-project.iccs.recommender.core/eu.alert-project.iccs.recommender.calculator/src/main/java/eu/alertproject/iccs/socrates/calculator/api/CosineSimilarityCalculator.java
+++ eu.alert-project.iccs.recommender.core/eu.alert-project.iccs.recommender.calculator/src/main/java/eu/alertproject/iccs/socrates/calculator/api/CosineSimilarityCalculator.java
@@ -1,11 +1,13 @@
 package eu.alertproject.iccs.socrates.calculator.api;
 
 import eu.alertproject.iccs.socrates.calculator.internal.model.AnnotatedObject;
+import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -28,7 +30,28 @@ public class CosineSimilarityCalculator implements SimilarityCalculator {
         //create a set with the words they have in common
         Set<String> commonWordsSet = new HashSet<String>(item1.getAnnotations().keySet());
         commonWordsSet.retainAll(item2.getAnnotations().keySet());
+        
+//                Iterator item1Itr =item1.getAnnotations().keySet().iterator();
+//        System.out.println("========Item 1 Words==============");
+//        while (item1Itr.hasNext()) {
+//            System.out.print(item1Itr.next() +" ");
+//        }
+//        
+//                        Iterator item2Itr =item2.getAnnotations().keySet().iterator();
+//        System.out.println("========Item 2 Words==============");
+//        while (item2Itr.hasNext()) {
+//            System.out.print(item2Itr.next() +" ");
+//        }
+//        
+//        
+//        
+//        Iterator commonWordSetItr = commonWordsSet.iterator();
+//        System.out.println("========Common Words==============");
+//        while (commonWordSetItr.hasNext()) {
+//            System.out.print(commonWordSetItr.next() +" ");
+//        }
 
+        System.out.println("Common words found " + commonWordsSet.size());
         //Calculates the cosine similarity. For the numerator we need only the common words, for the denominator the norms of the words found in each vector.
         double numerator = 0, norm1 = 0, norm2 = 0;
 
@@ -72,6 +95,57 @@ public class CosineSimilarityCalculator implements SimilarityCalculator {
                 norm1,
                 norm2,
                 similarity});
+
+        return similarity;
+
+    }
+   
+    public Double getIntegerSimilarity(HashMap<Integer, Integer> itemOne, HashMap<Integer, Integer> itemTwo) {
+        if ((itemOne == null) || (itemTwo == null)) {
+            throw new IllegalArgumentException("Items have not been initialized. Initialize the items and try again");
+        }
+        Set<Integer> commonWordsSet = new HashSet<Integer>(itemOne.keySet());
+        commonWordsSet.retainAll(itemTwo.keySet());
+
+        System.out.println("Common words found " + commonWordsSet.size());
+        //Calculates the cosine similarity. For the numerator we need only the common words, for the denominator the norms of the words found in each vector.
+        double numerator = 0, norm1 = 0, norm2 = 0;
+
+
+
+        if (commonWordsSet.size() <= 0
+                || itemOne.keySet().size() <= 0
+                || itemTwo.keySet().size() <= 0) {
+            return 0.0;
+        }
+
+        for (Integer k : commonWordsSet) {
+            numerator += itemOne.get(k) * itemTwo.get(k);
+        }
+
+
+
+        for (Integer k : itemOne.keySet()) {
+            norm1 += itemOne.get(k) * itemOne.get(k);
+        }
+        for (Integer k : itemTwo.keySet()) {
+            norm2 += itemTwo.get(k) * itemTwo.get(k);
+        }
+
+
+        //this could return NaN if either norm1 or norm2 are 0
+        if (norm1 <= 0.0 || norm2 <= 0.0) {
+            return 0.0;
+        }
+
+
+        Double similarity = numerator / Math.sqrt(norm1 * norm2);
+        logger.trace("Double getSimilarity() {}/Math.sqrt({}*{})={}",
+                new Object[]{
+                    numerator,
+                    norm1,
+                    norm2,
+                    similarity});
 
         return similarity;
 
