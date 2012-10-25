d82b3d0ea8689d341658d30d4f6e07cfe56a86c2
Fotis Paraskevopoulos <fotis@existanze.com>
2012-10-25 13:50:42 +0300

    Correcting indexes

--- eu.alert-project.iccs.recommender.core/eu.alert-project.iccs.recommender.datastore/src/main/db/migrations/20121022213222_indexes.sql
+++ eu.alert-project.iccs.recommender.core/eu.alert-project.iccs.recommender.datastore/src/main/db/migrations/20121022213222_indexes.sql
@@ -1,3 +1,3 @@
-create index component_subject_subject_idx on component_subject(subject);
-create index component_subject_idx on component_subject(component);
-create index component_uuid_idx on uuid_component(component);
+create index component_subject_subject_idx on component_subject(subject(255));
+create index component_subject_idx on component_subject(component(255));
+create index component_uuid_idx on uuid_component(component(255));
